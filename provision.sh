60f41719f339910b7eb18e6d4d78856e0610d68a
Per Nordlöw <per.nordlow@gmail.com>
2026-05-20 22:11:01 +0200

    Modify provision.sh

--- provision.sh
+++ provision.sh
@@ -241,11 +241,12 @@ if should_install "cuik"; then
     CUIK_TMP=$(mktemp -d)
     git clone --depth 1 https://github.com/RealNeGate/Cuik/ "$CUIK_TMP"
     pushd "$CUIK_TMP"
+	sed -i 's/-Werror//g' build.lua
+	find . -type f \( -name "*.c" -o -name "*.h" \) -exec sed -i 's/__debugbreak/__builtin_trap/g' {} +
 	sed -i '1i #include <ctype.h>' common/common.c
 	sed -i '1i #include <ctype.h>' tb/x64/x64_gen.h
 	sed -i '1i #include <stddef.h>' tb/libtb.c
 	sed -i 's/static TB_Node\* make_int_node/TB_Node\* make_int_node/g' tb/new_builder.c
-	find . -type f \( -name "*.c" -o -name "*.h" \) -exec sed -i 's/__debugbreak/__builtin_trap/g' {} +
 	CFLAGS="-D__debugbreak=__builtin_trap -include ctype.h" \
 		  luajit build.lua -x64 -driver -cuik -tb
 fi
